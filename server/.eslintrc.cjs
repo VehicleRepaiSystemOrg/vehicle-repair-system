c07c4b44030cf7c38ab8cf204497864a808017af
Thisan Mindiya Kulathilake <139038281+mindiya12@users.noreply.github.com>
2025-12-17 16:52:57 +0530

    ci: add lint/test/docker build pipeline

---
+++ server/.eslintrc.cjs
@@ -0,0 +1,5 @@
+module.exports = {
+  env: { node: true, es2022: true, jest: true },
+  extends: ["eslint:recommended", "prettier"],
+  ignorePatterns: ["node_modules/"],
+};
---
+++ server/app.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+
+const app = express();
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) =>
+  res.json({ message: "Vehicle Repair System API is running!" })
+);
+
+module.exports = app;
