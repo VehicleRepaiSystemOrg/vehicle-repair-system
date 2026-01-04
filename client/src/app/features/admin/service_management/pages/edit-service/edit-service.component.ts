f432d4ef9d6b913ab55704bc5440c9263e21ea76
mindiya12 <mindiyakulathilake648@gmail.com>
2026-01-04 21:45:37 +0530

    service added for admin dashboard

--- client/src/app/features/admin/service_management/pages/edit-service/edit-service.component.ts
+++ client/src/app/features/admin/service_management/pages/edit-service/edit-service.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject, signal, computed } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
