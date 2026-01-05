0f87553aa421d3510430311b914a5d426c53bacd
isuri54 <154318794+isuri54@users.noreply.github.com>
2026-01-05 15:05:37 +0530

    feat: Implement admin-client message option

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,28 +1,23 @@
 // eslint.config.js
+// @ts-check
 const eslint = require('@eslint/js');
 const tseslint = require('typescript-eslint');
-const angular = require('@angular-eslint/eslint-plugin');
+const angular = require('angular-eslint');
 
 module.exports = tseslint.config(
-  // Global ignores (optional but recommended)
-  {
-    ignores: ['**/*.spec.ts', 'projects/**/*', 'node_modules/**/*'],
-  },
-
-  // TypeScript files
   {
     files: ['**/*.ts'],
     languageOptions: {
       parserOptions: {
-        project: true,                  // Enables full TS parsing + decorators
-        tsconfigRootDir: __dirname,     // Critical for resolving tsconfig.json
+        project: true,
+        tsconfigRootDir: __dirname,
       },
     },
     extends: [
       eslint.configs.recommended,
       ...tseslint.configs.recommended,
       ...tseslint.configs.stylistic,
-      angular.configs.tsRecommended, // ← This one is an OBJECT, do NOT spread
+      angular.configs.tsRecommended, // ← object, NO spread
     ],
     processor: angular.processInlineTemplates,
     rules: {
@@ -36,13 +31,11 @@ module.exports = tseslint.config(
       ],
     },
   },
-
-  // HTML template files
   {
     files: ['**/*.html'],
     extends: [
-      angular.configs.templateRecommended,
-      angular.configs.templateAccessibility,
+      angular.configs.templateRecommended,     // ← object, NO spread
+      angular.configs.templateAccessibility,   // ← object, NO spread
     ],
     rules: {},
   }
