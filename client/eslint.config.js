33b578240f410bc132dbe0ceb700897aaaf9cdd5
isuri54 <154318794+isuri54@users.noreply.github.com>
2026-01-05 13:28:40 +0530

    feat: Implement admin-client message option

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,40 +1,41 @@
+// eslint.config.js
 const eslint = require('@eslint/js');
-const { defineConfig } = require('eslint/config');
 const tseslint = require('typescript-eslint');
-const angular = require('angular-eslint');
+const angular = require('@angular-eslint/eslint-plugin');
 
-module.exports = defineConfig([
+module.exports = tseslint.config(
   {
     files: ['**/*.ts'],
+    languageOptions: {
+      parserOptions: {
+        project: true, // This enables type-aware rules and decorator support
+        tsconfigRootDir: __dirname, // Important: sets the root dir for relative tsconfig paths
+      },
+    },
     extends: [
       eslint.configs.recommended,
-      tseslint.configs.recommended,
-      tseslint.configs.stylistic,
-      angular.configs.tsRecommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
     ],
     processor: angular.processInlineTemplates,
     rules: {
       '@angular-eslint/directive-selector': [
         'error',
-        {
-          type: 'attribute',
-          prefix: 'app',
-          style: 'camelCase',
-        },
+        { type: 'attribute', prefix: 'app', style: 'camelCase' },
       ],
       '@angular-eslint/component-selector': [
         'error',
-        {
-          type: 'element',
-          prefix: 'app',
-          style: 'kebab-case',
-        },
+        { type: 'element', prefix: 'app', style: 'kebab-case' },
       ],
     },
   },
   {
     files: ['**/*.html'],
-    extends: [angular.configs.templateRecommended, angular.configs.templateAccessibility],
+    extends: [
+      ...angular.configs.templateRecommended,
+      ...angular.configs.templateAccessibility,
+    ],
     rules: {},
-  },
-]);
+  }
+);
